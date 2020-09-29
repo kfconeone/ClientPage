3e5249c45514a27907be260850949d0912de6f7c
kfconeone <kfconeone@gmail.com>
2020-09-29 21:07:07 +0800

    init

---
+++ precache-manifest.d8284d18f1db76de2de05f32742ddb7c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec4ee1f6e0489d58f029",
+    "url": "/css/app.df1044ea.css"
+  },
+  {
+    "revision": "252812768d25449ae622",
+    "url": "/css/chunk-vendors.3ab74869.css"
+  },
+  {
+    "revision": "0a9f22b37a1462c9d7512cce264006f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b67aa19a0956fded576",
+    "url": "/js/about.f415d8e3.js"
+  },
+  {
+    "revision": "ec4ee1f6e0489d58f029",
+    "url": "/js/app.0ca73673.js"
+  },
+  {
+    "revision": "252812768d25449ae622",
+    "url": "/js/chunk-vendors.6746ee74.js"
+  },
+  {
+    "revision": "628e516c8fe0d9aa75b64eff15d3d956",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
